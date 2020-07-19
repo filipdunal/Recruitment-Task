407ba03114ad8c86a142309f7911b351763cac08
neo938 <neo938@gmail.com>
2020-07-20 01:35:01 +0200

    Initial commit

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] clips=new AudioClip[3];
+    public AudioSource audioSource;
+
+    public Toggle[] toggles = new Toggle[3];
+    
+    public void PlaySound()
+    {
+        List<int> activeToggles=new List<int>();
+        for(int i=0;i<toggles.Length;i++)
+        {
+            if(toggles[i].isOn)
+            {
+                activeToggles.Add(i);
+            }
+        }
+
+        if(activeToggles.Count>0)
+        {
+            int clipNumberToPlay = activeToggles[Random.Range(0, activeToggles.Count)];
+            audioSource.PlayOneShot(clips[clipNumberToPlay]);
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public GameObject text;
+    public void ShowText(float time)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShowTextCoroutine(time));
+    }
+    IEnumerator ShowTextCoroutine(float time)
+    {
+        text.SetActive(true);
+        yield return new WaitForSecondsRealtime(time);
+        text.SetActive(false);
+
+    }
+}
